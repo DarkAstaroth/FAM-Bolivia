77ab43a2977c153503aa92bb26ba407cfaa54126
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2019-08-17 01:11:49 -0400

    se agregaron las fuentes al pubspec y se agrego el titulo del onboarding

--- lib/onboarding/onboarding.dart
+++ lib/onboarding/onboarding.dart
@@ -10,20 +10,41 @@ class Onboarding extends StatelessWidget{
         body: Stack(
           children: <Widget>[
             Container(
+              
               decoration: BoxDecoration(
                 color: Colors.blue
               ),
             ),
             Container(
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text("Bienvenido"),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Text(
+                      "Bienvenido",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: "LatoBold"
+                      ),
+                    ),                   
+                  ),
+                Padding(
+                  padding: EdgeInsets.only(right: 30),
+                  child: Text(
+                    "Omitir",
+                    style: TextStyle(
+                      fontFamily: "LatoRegular",
+                      fontSize: 15
+                    ),
+                    ),
+                )
                 ],
               ),
               width: double.infinity,
               height: 75,
               decoration: BoxDecoration(
-                color: Colors.red
+                color: Colors.white
               ),
             ),
             
