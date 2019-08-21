3bf2fbc1a2997701e6abd901fc3213574d277a72
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2019-08-21 11:22:31 -0400

    se agrego un page view para las paginas principales y la funcionalidad la bottom navy bar

--- lib/Pages/famApp.dart
+++ lib/Pages/famApp.dart
@@ -2,57 +2,71 @@ import 'package:fam_bolivia/Pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
-class FamApp extends StatefulWidget{
-  State<StatefulWidget> createState(){
+class FamApp extends StatefulWidget {
+  State<StatefulWidget> createState() {
     return _FamApp();
   }
 }
 
-class _FamApp extends State<FamApp>{
-  
-  int indextap=0;
+class _FamApp extends State<FamApp> {
+  final PageController pageController = PageController();
+  int indextap = 0;
 
-  final List<Widget> widgetsChildren=[
+  final List<Widget> widgetsChildren = [
+    HomeScreen(),
     HomeScreen(),
     HomeScreen(),
     HomeScreen()
   ];
-  void onTapTapped(int index){
+  void onTapTapped(int index) {
     setState(() {
-      indextap=index;
+      indextap = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
-      body: widgetsChildren[indextap] ,
-      bottomNavigationBar: BottomNavyBar(
-        selectedIndex: indextap,
-        showElevation: true,
-        onItemSelected: (index)=> setState((){
-          indextap=index;
-          onTapTapped(index);
-        }),
-        items: [
-          BottomNavyBarItem(
-            icon: Icon(Icons.book),
-            title: Text("Directorio"),
-            activeColor: Color(0xff004fa3),
-          ),
-           BottomNavyBarItem(
-            icon: Icon(Icons.phone),
-            title: Text("Contactos"),
-            activeColor: Color(0xff004fa3)
-          ),
-           BottomNavyBarItem(
-            icon: Icon(Icons.info),
-            title: Text("Ayuda"),
-            activeColor: Color(0xff004fa3)
-          ),
-        ],
-      )
-    );
+        body: PageView.builder(
+          controller: pageController,
+          onPageChanged: (index) {
+            setState(() {
+              indextap = index;
+            });
+          },
+          itemBuilder: (BuildContext builderContext, int index) =>
+              widgetsChildren[index],
+          itemCount: widgetsChildren.length,
+        ),
+        bottomNavigationBar: BottomNavyBar(
+          selectedIndex: indextap,
+          showElevation: true,
+          onItemSelected: (index) => setState(() {
+            indextap = index;
+            pageController.animateToPage(index,
+                duration: Duration(milliseconds: 300), curve: Curves.ease);
+          }),
+          items: [
+            BottomNavyBarItem(
+              icon: Icon(Icons.book),
+              title: Text("Directorio"),
+              activeColor: Color(0xff004fa3),
+            ),
+            BottomNavyBarItem(
+              icon: Icon(Icons.search),
+              title: Text("Buscar"),
+              activeColor: Color(0xff004fa3),
+            ),
+            BottomNavyBarItem(
+                icon: Icon(Icons.phone),
+                title: Text("Contactos"),
+                activeColor: Color(0xff004fa3)),
+            BottomNavyBarItem(
+                icon: Icon(Icons.info),
+                title: Text("Ayuda"),
+                activeColor: Color(0xff004fa3)),
+          ],
+        ));
   }
-
-
 }
