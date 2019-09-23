2844eeb92653c0137f39244c44b80c067670fbdf
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2019-09-23 09:10:00 -0400

    se agrego un gradiente en el header principal de la app

--- lib/Pages/home/header-home.dart
+++ lib/Pages/home/header-home.dart
@@ -8,7 +8,17 @@ class HeaderHome extends StatelessWidget {
         height: double.infinity,
         width: double.infinity,
         decoration: BoxDecoration(
-          color: Color(0xff004fa3),
+          
+          gradient: LinearGradient(
+            colors: [
+              Colors.white,
+              Color(0xff004fa3)
+            ],
+            tileMode: TileMode.clamp,
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0,1.0],
+          )
         ),
         child: Stack(
           children: <Widget>[
@@ -32,16 +42,30 @@ class HeaderHome extends StatelessWidget {
                 )),
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(0),
+                  child:
+                  Container(
+                  width: 150,
+                  height: 180,
+                  
+                  child: Container(
+                    padding: EdgeInsets.all(30),
+                    child: Image(image: AssetImage("assets/images/logofam.png"),),
+                  ),
+                ),
+                ),
                 Expanded(
                   child: Container(
-                    height: 180,
+                    height: 190,
                     child: Center(
                       child: Text(
                         "Directorio de Asociaciones\nMunicipales de Bolivia",
                         style: TextStyle(
                             color: Colors.white,
-                            fontSize: 20,
+                            fontSize: 16,
                             fontFamily: "LatoBold"),
                         textAlign: TextAlign.center,
                       ),
